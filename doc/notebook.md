696324daf13403b0bec0b3c42bd2e2d0569e5dbd
Li Wu <catrina.wuli@gmail.com>
2022-05-21 23:19:25 +0200

    create report including edges and degree distributions for 1000000 records

--- doc/notebook.md
+++ doc/notebook.md
@@ -6,3 +6,6 @@ create java main class to read file.
 
 # 2022-05-20
 add filter to a limited sized data(10000) for length larger than 1000.
+
+# 2022-05-21
+create report including edges and degree distributions for 1000000 records.
--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -6,29 +6,83 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
 public class Graph {
 
+	private int vertices;
+
+	private int edges;
+
+	private Map<String, List<String>> adjacencyTable;
+
+	public Graph() {
+		adjacencyTable = new HashMap<String, List<String>>();
+	}
+
+	private void addVertice(String id) {
+		if (this.adjacencyTable.get(id) == null) {
+			List<String> list = new ArrayList<String>();
+			this.adjacencyTable.putIfAbsent(id, list);
+			this.vertices++;
+		}
+	}
+
+	public void addEdge(String id1, String id2) {
+		addVertice(id1);
+		addVertice(id2);
+		this.adjacencyTable.get(id1).add(id2);
+		this.adjacencyTable.get(id2).add(id1);
+		this.edges++;
+	}
+
+	public int getVertices() {
+		return this.vertices;
+	}
+
+	public int getEdges() {
+		return this.edges;
+	}
+
+	public void histogram()	{
+		int max = 10; //return the first 10
+		int index = 1;
+		System.out.println("Histogram by the first " + max);
+		for (Map.Entry<String, List<String>> entry : adjacencyTable.entrySet()) {
+			if (index++ > max) {
+				break;
+			}
+			System.out.println(entry.getKey() + " : " + entry.getValue().size());
+		}
+	}
+
 	public static void main(String[] args) {
 		try {
+			Graph graph = new Graph();
 			InputStream fileStream = new FileInputStream("overlaps.m4.gz");
 			InputStream gzipStream = new GZIPInputStream(fileStream);
 			Reader decoder = new InputStreamReader(gzipStream, "ASCII");
 			BufferedReader buffered = new BufferedReader(decoder);
 
-			int max = 10000;
+			int max = 1000000; // 1M
 			String line;
-			for (int index = 0; (line = buffered.readLine()) != null && index < max; index++) {
+			for (int index = 0; (line = buffered.readLine()) != null && index < max ; index++) {
 				String[] array = line.split("\\s+");
 				String id1 = array[0];
 				String id2 = array[1];
 				int lengthOfFirstContig = Integer.parseInt(array[7]);
 				int lengthOfSecondContig = Integer.parseInt(array[11]);
 				if (lengthOfFirstContig >= 1000 && lengthOfSecondContig >= 1000) {
-					System.out.println(id1 + " " + id2 + " " + lengthOfFirstContig + " " +lengthOfSecondContig);
+//					System.out.println(id1 + " " + id2 + " " + lengthOfFirstContig + " " +lengthOfSecondContig);
+					graph.addEdge(id1, id2);
 				}
 			}
+			System.out.println("Total vertices: " + graph.getVertices() + " total edges: " + graph.getEdges() + " for first " + max + " records");
+			graph.histogram();
 			buffered.close();
 		}
 		catch (IOException e) {
